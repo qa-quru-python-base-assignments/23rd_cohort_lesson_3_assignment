f17efc3535ebd4aa59e45a830c2719b2280fad9f
Waldemar <vladdigen@icloud.com>
2025-12-17 01:35:09 +0400

    complete assigment

---
+++ test_main.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def driver():
+    opts = Options()
+
+    opts.add_argument("--headless=new")
+    opts.add_argument("--window-size=1280,900")
+    driver = webdriver.Chrome(options=opts)
+    yield driver
+
+    driver.quit()
+
+
+def test_google_web(driver):
+    url = "https://www.google.com/"
+    driver.get(url)
+    assert driver.title == "Google"
+    assert driver.current_url == url
+
+
+def test_github_web(driver):
+    url = "https://github.com/"
+    driver.get(url)
+    assert driver.title == "GitHub · Change is constant. GitHub keeps you ahead. · GitHub"
+    assert driver.current_url == url
